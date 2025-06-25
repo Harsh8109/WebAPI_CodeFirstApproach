09d393bb0a3d261abf505fcb278a7373670d6f2b
HARSH <HARSH@DESKTOP-25EQF6U>
2025-06-25 10:07:52 +0530

    Removed XML formatter to see response in JSON

--- WeAPICodeFirstDemo/App_Start/WebApiConfig.cs
+++ WeAPICodeFirstDemo/App_Start/WebApiConfig.cs
@@ -14,6 +14,9 @@ namespace WeAPICodeFirstDemo
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //Remove the XML formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
