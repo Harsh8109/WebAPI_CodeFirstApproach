89866f79b7078597cf8c790fcc58c6e019055da7
HARSH <HARSH@DESKTOP-25EQF6U>
2025-06-26 00:17:14 +0530

    Added Jquery AJAX for UI CRUD ops

---
+++ WeAPICodeFirstDemo/WebAPIClient/main.js
@@ -0,0 +1,47 @@
+﻿function createEmployee() {
+    var url = "/api/Employee";
+    var employee = {};
+
+    if ($("#txtFirstName").val() === '' || $("#txtLastName").val() === '' || $("#txtGender").val() === '' ||
+        $("#txtCity").val() === '' || $("#txtIsActive").val() === '') {
+        alert("Please fill all fields.");
+    }
+    else {
+        employee.FirstName = $("#txtFirstName").val();
+        employee.LastName = $("#txtLastName").val();
+        employee.Gender = $("#txtGender").val();
+        employee.City = $("#txtCity").val();
+        employee.IsActive = $("#txtIsActive").val();
+
+        // Make an Jquery's AJAX call to create the employee
+        if (employee) {
+            $.ajax({
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: JSON.stringify(employee),
+                type: "POST",
+                success: function (result)
+                {
+                    clearForm();
+                },
+
+            
+                error: function (msg)
+                {
+                    alert(msg);
+                }
+
+            });
+        }
+    }
+}
+
+function clearForm() {
+    $("#txtFirstName").val('');
+    $("#txtLastName").val('');
+    $("#txtGender").val('');
+    $("#txtCity").val('');
+    $("#txtIsActive").val('');
+
+}
